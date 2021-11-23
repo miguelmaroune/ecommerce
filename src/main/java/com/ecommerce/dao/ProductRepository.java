c9569db1a12deac78cc25aaf6bd7eafd84a4770e
miguelmaroune <miguelmaroune@gmail.com>
2021-11-23 19:52:30 +0200

    release 2.0 updates

--- src/main/java/com/ecommerce/dao/ProductRepository.java
+++ src/main/java/com/ecommerce/dao/ProductRepository.java
@@ -1,12 +1,17 @@
 package com.ecommerce.dao;
 
 import com.ecommerce.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @CrossOrigin("http://localhost:4200")
 //Spring Data Rest will expose /products repo.
 public interface ProductRepository extends JpaRepository<Product, Long>
 {
 
+    Page<Product> findByCategoryId(@RequestParam("id") Long id , Pageable pageable);
 }
